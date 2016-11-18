0953bc46a5621bf2d1ea22170376ec092f37fc33
Patrik Dudits <patrik@dudits.net>
2016-11-18 23:29:38 +0100

    Pridane vyhladavacie policko

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 import { SearchComponent } from './search.component';
 
 export { SearchComponent } from './search.component';
@@ -9,6 +10,6 @@ export { SearchComponent } from './search.component';
   imports: [
     CommonModule
   ],
-  declarations: [ProductListComponent]
+  declarations: [ProductListComponent, SearchBarComponent, SearchComponent]
 })
 export class SearchModule { }
