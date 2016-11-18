0367332cd6858d220194c824fdff30921ece299e
Patrik Dudits <patrik@dudits.net>
2016-11-18 22:35:45 +0100

    ProductList - vysledok vyhladavania
    ng g component search/product-list

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ProductListComponent]
 })
 export class SearchModule { }
