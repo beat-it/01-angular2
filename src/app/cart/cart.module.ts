7c69e7a77da75f93af1b55f57e9579aa2f2fb101
Patrik Dudits <patrik@dudits.net>
2016-11-28 23:21:39 +0100

    Vnorený router.
    Pre košík budeme potrebovať tri rôzne obrazovku so spoločnou navigáciou.
    Vždy bude zobrazená navigácia od CartComponent, a obsah vnútri vnoreného router-outlet.

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -3,18 +3,31 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { CartComponent } from './cart.component';
 import { CartWidgetComponent } from './cart-widget/cart-widget.component';
+import { CartItemsComponent } from './cart-items/cart-items.component';
 
 
-// const routes:Routes = [
-//   {path: '/cart', component: }
-// ];
+const routes: Routes = [
+    {
+        path: 'cart',
+        component: CartComponent,
+        // CartComponent obsahuje direktívu <router-outlet>. Jej obsah je
+        // riadený URL za cart/ nasledovne:
+        children: [
+            { path: '', pathMatch: 'full', redirectTo: 'items' },
+            { path: 'items', component: CartItemsComponent },
+        ],
+    }
+];
+
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  exports: [CartWidgetComponent],
-  declarations: [CartComponent,
-    CartWidgetComponent],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes)
+    ],
+    exports: [CartWidgetComponent],
+    declarations: [CartComponent,
+        CartWidgetComponent,
+        CartItemsComponent],
 })
 export class CartModule { }
