e250404d99d28ad4fac564729620d43e670a1a7c
Patrik Dudits <patrik@dudits.net>
2016-11-29 00:27:33 +0100

    Vzorové dáta pre košík.
    Aj keď backend vyzerá vcelku dokončene, ešte vždy je jednoduchšie robiť bez neho.

--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@angular/core';
 import { ReadCartResponse, AddToCartRequest, AddToCartResponse,
-  CartInfoResponse, Response, CheckoutOptionsResponse , CartInfo} from '../../api';
+  CartInfoResponse, Response, CheckoutOptionsResponse , CartItem, CartInfo} from '../../api';
 import { delay } from '../util';
 import { Observable, BehaviorSubject } from 'rxjs/Rx';
+
+const placeholder_image = {
+  url: "http://placehold.it/400x500/ffffff",
+  thumbnail_url: "http://placehold.it/80x60/ffffff"
+};
+
+const sampleData:CartItem[] = [
+  {
+    id: "01",
+    name: "ZiZi",
+    price: 150,
+    currency: "EUR",
+    rating: 3,
+    image: placeholder_image,
+    quantity: 5,
+    total_price: 150*5,
+  },
+  {
+    id: "02",
+    name: "Kiwi J",
+    price: 250,
+    currency: "EUR",
+    rating: 2,
+    image: placeholder_image,
+    quantity: 4,
+    total_price: 4*250,
+  },
+  {
+    id: "03",
+    name: "Angular 2",
+    price: 50,
+    currency: "EUR",
+    rating: 5,
+    image: placeholder_image,
+    quantity: 2,
+    total_price: 2*50,
+  },
+];
 
 @Injectable()
 export class CartService {
@@ -13,7 +51,7 @@ export class CartService {
   public cartInfo$: Observable<CartInfo> = this._cartInfo;
 
   readCart():Promise<ReadCartResponse> {
-    return delay({status:"OK", data:[]});
+    return delay({status:"OK", data:sampleData});
   }
 
   addToCart(req: AddToCartRequest):Promise<AddToCartResponse> {
