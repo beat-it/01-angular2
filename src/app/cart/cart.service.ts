d6cf4a454d2c8e30965fdc2d9639f57ba8b0b131
Patrik Dudits <patrik@dudits.net>
2016-11-28 22:17:19 +0100

    Reactive Javascript - RxJS. Ďalší stavebný kameň Angular 2.
    Zatiaľ čo takmer štandardný javascriptový Promise opisuje jednu hodnotu, ktorú možno získame, Observable opisuje postupnosť asynchrónne získaných hodnôt.
    V UI to umožní naviazať zobrazenie tej istej hodnoty vo viacerých komponentoch, nezávisle od miest ktoré môžu spôsobiť zmenu tejto hodnoty.
    Ak konzistentne  použijeme Observables ako jediný zdroj dát v aplikácii, vznikne architektúra podobná Redux. Pozri tiež ngrx/store.

--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ReadCartResponse, AddToCartRequest, AddToCartResponse,
-  CartInfoResponse, Response, CheckoutOptionsResponse } from '../../api';
+  CartInfoResponse, Response, CheckoutOptionsResponse , CartInfo} from '../../api';
 import { delay } from '../util';
+import { Observable, BehaviorSubject } from 'rxjs/Rx';
 
 @Injectable()
 export class CartService {
 
   constructor() { }
+
+  private _cartInfo = new BehaviorSubject<CartInfo>({item_count: 0, total_price: 0});
+  public cartInfo$: Observable<CartInfo> = this._cartInfo;
 
   readCart():Promise<ReadCartResponse> {
     return delay({status:"OK", data:[]});
@@ -17,7 +21,8 @@ export class CartService {
   }
 
   cartInfo():Promise<CartInfoResponse> {
-    return delay({status:"OK", data: {item_count:3, total_price:10}});
+    return delay({status:"OK", data: {item_count:3, total_price:10}})
+      .then((response) => {this._cartInfo.next(response.data); return response});
   }
 
   removeFromCart(product_id:string):Promise<Response> {
